ba1d574965d2a6e85a9973188544d0a83e2a8f23
lisandroaloo <alolisandro02@gmail.com>
2025-06-29 15:31:43 -0300

    fix

--- src/main/java/com/chefcito/chefcitobackend/dto/RequestRecipeDto.java
+++ src/main/java/com/chefcito/chefcitobackend/dto/RequestRecipeDto.java
@@ -1,6 +1,7 @@
 package com.chefcito.chefcitobackend.dto;
 
 import com.chefcito.chefcitobackend.model.Recipe;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,18 +12,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class RequestRecipeDto {
+
+  @JsonProperty("us_id")
   private Long re_creator_us_id;
+
+  @JsonProperty("picture")
   private String re_picture;
+
+  @JsonProperty("title")
   private String re_title;
+
+  @JsonProperty("vegan")
   private Boolean re_suitable_for_vegan;
+
+  @JsonProperty("vegetarian")
   private Boolean re_suitable_for_vegetarian;
+
+  @JsonProperty("celiac")
   private Boolean re_suitable_for_celiac;
+
+  @JsonProperty("lactose")
   private Boolean re_suitable_for_lactose_intolerant;
 
-  public static Recipe toRecipe(RequestRecipeDto requestRecipeDto) {
-    return new Recipe(null, null, requestRecipeDto.getRe_picture(), requestRecipeDto.getRe_title(),
-        requestRecipeDto.getRe_suitable_for_vegan(), requestRecipeDto.getRe_suitable_for_vegetarian(),
-        requestRecipeDto.getRe_suitable_for_celiac(), requestRecipeDto.getRe_suitable_for_lactose_intolerant(),
-        null, null);
+  public static Recipe toRecipe(RequestRecipeDto dto) {
+    return new Recipe(
+      null, // id
+      null, // creator (lo podrías setear usando re_creator_us_id con el repositorio)
+      dto.getRe_picture(),
+      dto.getRe_title(),
+      dto.getRe_suitable_for_vegan(),
+      dto.getRe_suitable_for_vegetarian(),
+      dto.getRe_suitable_for_celiac(),
+      dto.getRe_suitable_for_lactose_intolerant(),
+      null, // ingredientes
+      null  // pasos
+    );
   }
-} 
+}
